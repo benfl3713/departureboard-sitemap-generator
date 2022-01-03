aa44cd85cab69cf8c0652ef78c0ffb90419161a3
benfl3713 <buses3713@gmail.com>
2022-01-03 20:52:18 +0000

    Added namespace

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const axios = require("axios").default;
 const X2JS = require("x2js");
 const fs = require("fs");
 
+const fileName = "./sitemap_stations.xml";
+
+// Removes existing file if it exists
+if(fs.existsSync(fileName)){
+    console.log("Deleting Existing file: " + fileName);
+    fs.rmSync(fileName, {maxRetries: 4});
+}
+
 axios.get("https://api.leddepartureboard.com/api/StationLookup")
     .then(data => processStations(data.data))
 
@@ -10,9 +18,11 @@ function processStations(jsonData) {
     const codes = jsonData.map(s => s.code);
     const sitemap = {
         urlset: {
-            url: codes.map(c => {return {loc: `https://www.leddepartureboard.com/${c}`}})
+            url: codes.map(c => {return {loc: `https://www.leddepartureboard.com/${c}`}}),
+            "_xmlns": "http://www.sitemaps.org/schemas/sitemap/0.9"
         }
     }
 
     fs.writeFileSync("./sitemap_stations.xml", new X2JS().js2xml(sitemap))
+    console.log("\nFinished Generating sitemap. Outputted result to: " + fileName)
 }
